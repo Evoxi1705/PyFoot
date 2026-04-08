c1701c55334f1c5e767b6e86dff898ceaaa75091
ninaforrez <Nina.Forrez@vub.be>
2026-04-08 09:22:19 +0200

    Update wall_class.py

--- wall_class.py
+++ wall_class.py
@@ -4,11 +4,11 @@ from constants import WALL_COLOR
 
 class Wall(StaticObject):
     def __init__(self, pos, width, height, color=WALL_COLOR):
-        super.__init__(pos, height, width)
+        super().__init__(pos, height, width)
         self.color = color
         
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(screen, self.color, (self.pos.x, self.pos.y, self.width, self.height))
         
 
 
