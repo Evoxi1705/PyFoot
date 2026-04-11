f84468f90e6c334432368c05672a54586e435103
ninaforrez <Nina.Forrez@vub.be>
2026-04-11 13:10:14 +0200

    Update wall_class.py

--- wall_class.py
+++ wall_class.py
@@ -1,15 +1,13 @@
 import pygame
 
 class Wall:
-    def __init__(self,x,y,width,height,color=(255,255,255)):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height 
+    def __init__(self,x,y,width,height,color=(255,255,255), radius=20):
+        self.rect = pygame.Rect(x, y, width, height) #door dit te gebruiken zal de botsing functie in ball_class makkelijker zijn
         self.color = color
+        self.radius = radius #pas op deze moet kleiner zijn dan de helft van de korste zijde van de muur
         
     def draw(self,screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(screen, self.color, self.rect, border_radius = self.radius)
         
 
 
