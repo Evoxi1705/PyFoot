bb19c68a3db16dc8a2f19ca83596576bc85029c3
ninaforrez <Nina.Forrez@vub.be>
2026-04-11 11:44:15 +0200

    Update ball_class.py

--- ball_class.py
+++ ball_class.py
@@ -1,22 +1,25 @@
-import base_classes
+from base_classes import *
 import pygame 
 
-class Ball:
-    def __init__(self,pos,velocity, radius, bounce):
-        super().__init__(pos,velocity)
+class Ball(DynamicObject):
+    def __init__(self, pos, velocity, radius, bounce_factor):
+        super().__init__(pos, radius*2, radius*2, velocity)
         self.radius = radius
-        self.bounce = bounce
+        self.bounce_factor = bounce_factor
         
     def draw(self,screen):
-        pygame.draw.circle(screen,self.radius)
+        pygame.draw.circle(screen, (0,255,0), (self.pos.x, self.pos.y), self.radius)
         
     def bounce(self):
-        self.velocity *= -1
+        if ...:
+            self.velocity.y *= -self.bounce_factor
+        elif ...:
+            self.velocity.x *= -self.bounce_factor
+        
+        
 
         
         
-        
-        
-        
+       
         
         
