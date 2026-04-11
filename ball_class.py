9704ce6d05eec17ad880dbab4d440056888234fc
ninaforrez <Nina.Forrez@vub.be>
2026-04-11 13:13:30 +0200

    Update ball_class.py

--- ball_class.py
+++ ball_class.py
@@ -2,6 +2,9 @@ from base_classes import *
 import pygame 
 
 class Ball(DynamicObject):
+    """
+    Class defining the ball
+    """
     def __init__(self, pos, velocity, radius, bounce_factor):
         super().__init__(pos, radius*2, radius*2, velocity)
         self.radius = radius
