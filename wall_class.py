338761577a172127b2ff01d694c5e05e13824aa1
ninaforrez <Nina.Forrez@vub.be>
2026-04-16 13:43:12 +0200

    Update wall_class.py

--- wall_class.py
+++ wall_class.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Wall:
-    def __init__(self,x,y,width,height,color=(255,255,255), radius=20):
+    def __init__(self,x,y,width,height, color, radius=20):
         self.rect = pygame.Rect(x, y, width, height) #door dit te gebruiken zal de botsing functie in ball_class makkelijker zijn
         self.color = color
         self.radius = radius #pas op deze moet kleiner zijn dan de helft van de korste zijde van de muur
@@ -11,3 +11,22 @@ class Wall:
         
 
 
+#drawing four blocks in each corner to shape my terrain     
+        
+field = Wall(80, 60, 520, 300, color=(255,255,255))
+
+
+block_top_left     = Wall(80,  60,  60, 100, color=(0,0,0))
+block_bottom_left  = Wall(80,  260, 60, 100, color=(0,0,0))
+block_top_right    = Wall(540, 60,  60, 100, color=(0,0,0))
+block_bottom_right = Wall(540, 260, 60, 100, color=(0,0,0))
+
+
+"""
+in game loop:
+field.draw(screen)
+block_top_left.draw(screen)
+block_bottom_left.draw(screen)
+block_top_right.draw(screen)
+block_bottom_right.draw(screen)
+"""
