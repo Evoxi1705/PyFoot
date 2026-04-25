1003844ad70d9d39a640ba7380cf9a02b56aea1a
ninaforrez <Nina.Forrez@vub.be>
2026-04-25 12:07:43 +0200

    up

--- PyFoot.py
+++ PyFoot.py
@@ -5,6 +5,18 @@ pygame.init()
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# For testing purposes
+class TempField:
+    def get_top(self): return 0
+    def get_bottom(self): return SCREEN_HEIGHT
+    def get_left(self): return 0
+    def get_right(self): return SCREEN_WIDTH
+
+#field = [block_top_left, block_bottom_left, block_top_right, block_bottom_right]
+
+field = TempField()
+
 pygame.display.set_caption("Main Menu")
 
 # Create a Rect object: (x, y, width, height)
